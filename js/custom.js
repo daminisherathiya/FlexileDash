2eb8aae2c438cfa6e1698a6996fc442216086eaf
Damini Sherathiya <daminisherathiya@gmail.com>
2021-04-09 16:30:47 +0530

    Added Sections

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@ var toggle_minimize = document.querySelectorAll(".toggle-minimize");
 var left_sidebar = document.querySelector(".left-sidebar");
 var logo_text = document.querySelector(".logo-text");
 var navbar_header = document.querySelector(".navbar-header");
+var sidbar_items = document.querySelectorAll(".sidebar-item");
 
 
 // minimize-sidebar
@@ -17,4 +18,14 @@ function toggle_sidebar() {
 window.addEventListener("resize", myFunction);
 function myFunction() {
     left_sidebar.classList.remove("small-sidebar");
+}
+
+sidbar_items.forEach(element => {
+    element.addEventListener("click", change_active_class);
+});
+function change_active_class(event) {
+    sidbar_items.forEach(element => {
+        element.querySelector('.sidebar-link').classList.remove("active");
+    });
+    event.currentTarget.querySelector('.sidebar-link').classList.add("active");
 }
