56c1a1adf78d6c29e85e96b304767182e9c75055
Damini Sherathiya <daminisherathiya@gmail.com>
2021-04-12 16:20:15 +0530

    Fixes

--- js/custom.js
+++ js/custom.js
@@ -3,21 +3,23 @@ var left_sidebar = document.querySelector(".left-sidebar");
 var logo_text = document.querySelector(".logo-text");
 var navbar_header = document.querySelector(".navbar-header");
 var sidbar_items = document.querySelectorAll(".sidebar-item");
-
+var close_btn = document.querySelector(".close-btn");
 
 // minimize-sidebar
 toggle_minimize.forEach(element => {
     element.addEventListener("click", toggle_sidebar);
 });
-
+close_btn.addEventListener("click", toggle_sidebar);
 function toggle_sidebar() {
     left_sidebar.classList.toggle("small-sidebar");
+    close_btn.classList.toggle("d-none");
 }
 
  // resize sidebar
 window.addEventListener("resize", myFunction);
 function myFunction() {
     left_sidebar.classList.remove("small-sidebar");
+    close_btn.classList.add("d-none");
 }
 
 sidbar_items.forEach(element => {
